2728a6bcec8668f8c95edfbad99b4c154298f642
Andrei <Andrei@DESKTOP-0LU8SBM>
2025-03-15 19:10:46 +0200

    Updated code, removed some formating mistakes

--- Vlad_AndreiCosmin_ActivitateSD2025/ExSeminar02.c
+++ Vlad_AndreiCosmin_ActivitateSD2025/ExSeminar02.c
@@ -1,19 +1,19 @@
 ////DACA VRETI SA RULATI, ADAUGATI FUNCTIA IN Seminar02.c IMPREUNA CU APELUL PENTUR MAIN
 //
 ////FUNCTIE
-//struct Masina getPrimaMasinaDupaModel(struct Masina* vector, int nrElemente, const char* conditie) {
-//
-//	for (int i = 0; i < nrElemente; i++)
-//	{
-//		if (strcmp(vector[i].model, conditie) == 0)
-//			return vector[i];
-//	}
-//	return initializare(0, NULL, 0, '-');
-//}
-//
-////APEL
-//printf("\n Prima masina: \n");
-//struct Masina primaMasina = getPrimaMasinaDupaModel(vectorMasini, nrMasini, "Audi");
-//afisare(primaMasina);
-//
-//dezalocare(&vectorMasini, &nrMasini);
+struct Masina getPrimaMasinaDupaModel(struct Masina* vector, int nrElemente, const char* conditie) {
+
+	for (int i = 0; i < nrElemente; i++)
+	{
+		if (strcmp(vector[i].model, conditie) == 0)
+			return vector[i];
+	}
+	return initializare(0, NULL, 0, '-');
+}
+
+//APEL
+printf("\n Prima masina: \n");
+struct Masina primaMasina = getPrimaMasinaDupaModel(vectorMasini, nrMasini, "Audi");
+afisare(primaMasina);
+
+dezalocare(&vectorMasini, &nrMasini);

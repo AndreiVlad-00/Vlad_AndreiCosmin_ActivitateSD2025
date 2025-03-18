c2e50ef5d91c1b54a41b5676d78e798730a2d062
Andrei <Andrei@DESKTOP-0LU8SBM>
2025-03-18 07:25:05 +0200

    definire structura

---
+++ Vlad_AndreiCosmin_ActivitateSD2025/Task1_StructuraSiFunctii.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Nume -> Vlad / Prenume -> Andrei Cosmin / Articol cu initialele V A C -> Avocat
+
+
+//definirea structurii cu cel putin 3 atribute, dintre care unul sa fie alocat dinamic
+typedef struct Avocat {
+	char* nume;
+	int varsta;
+	int aniExperienta;
+	float salariuAnual;
+	char* specializare;
+}Avocat;
+
+
+//functia de creare obiect Avocat
+
+Avocat creareAvocat() {
+
+}
+
+int main() {
+
+
+
+
+	return 0;
+}
+
+
+
